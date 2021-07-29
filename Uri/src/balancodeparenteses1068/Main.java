939bb67e339be2758d792f901cc866c8ec2ab9ec
Emanuelle Leães Alves <51135210+emanuellela@users.noreply.github.com>
2021-07-29 04:16:44 -0300

    UriOnlineJudge

--- Uri/src/balancodeparenteses1068/Main.java
+++ Uri/src/balancodeparenteses1068/Main.java
@@ -1,7 +1,6 @@
 package balancodeparenteses1068;
 import java.io.IOException;
 import java.util.Scanner;
-
 public class Main 
 {
 	public static void main(String[] args) throws IOException 
@@ -16,27 +15,27 @@ public class Main
 				for (int i = 0; i < input.length(); i++) 
 				{
 					char character = input.charAt(i);
-		            if(character == ')')
-		            {
-		            	if(abrirParenteses == 0)
-		            	{
-		            		throw new Exception();
-		            	}
-		            	abrirParenteses--;
-		            }
-		            if(character == '(') 
-		            {
-		            	abrirParenteses++;
-		            }
+					if(character == ')')
+				   	{
+						if(abrirParenteses == 0)
+						{
+							throw new Exception();
+						}
+						abrirParenteses--;
+				   	 }
+					 if(character == '(') 
+					 {
+					  	abrirParenteses++;
+					 }
 				}
 				if (abrirParenteses > 0) 
 				{
 					System.out.println("incorrect\n");
-		        }
+		       		}
 				else 
-		        {
+		      		{
 					System.out.println("correct\n");
-		        }
+		      		}
 			} 
 			catch(Exception ex) 
 			{
