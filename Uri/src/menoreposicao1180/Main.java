0034d70f9e46217b240e120f1ac97a484ad7e12f
Emanuelle Leães Alves <51135210+emanuellela@users.noreply.github.com>
2021-07-29 04:29:46 -0300

    UriOnlineJudge

--- Uri/src/menoreposicao1180/Main.java
+++ Uri/src/menoreposicao1180/Main.java
@@ -1,9 +1,7 @@
 package menoreposicao1180;
 import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
 	public static void main(String[] args) throws IOException
 	{
 		Scanner entrada = new Scanner(System.in);
@@ -13,18 +11,17 @@ public class Main {
 		for (int i = 0; i < N; i++) 
 		{
 			valor = entrada.nextInt();
-	        vetor[i] = valor;
-	    }
-	    mv = vetor[0];
-	    
-	    for (int X = 1; X < N; X++) 
-	    {
-	    	if (vetor[X] < mv) 
-	    	{
-	    		mv = vetor[X];
-	    		posicao = X;
-	        }
-	    }
-	    System.out.println("Menor valor: "+ mv +"\n"+ "Posicao: "+ posicao);
+	        	vetor[i] = valor;
+		}
+		mv = vetor[0];
+		for (int X = 1; X < N; X++) 
+		{
+			if (vetor[X] < mv) 
+			{
+				mv = vetor[X];
+				posicao = X;
+			}
+	    	}
+	    	System.out.println("Menor valor: "+ mv +"\n"+ "Posicao: "+ posicao);
 	}
 }
