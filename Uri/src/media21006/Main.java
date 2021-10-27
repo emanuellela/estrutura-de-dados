815ad91498be0a61aad46a0c8fd703f56416b676
1901570182 <emanuelleleaes@gmail.com>
2021-10-27 10:14:24 -0300

    Bônus

---
+++ Uri/src/media21006/Main.java
@@ -0,0 +1,16 @@
+package media21006;
+import java.util.Scanner;
+public class Main 
+{
+    public static void main(String[] args)
+    {
+    	Scanner entrada = new Scanner(System.in);
+		double n1 = entrada.nextDouble();
+		double n2 = entrada.nextDouble();
+		double n3 = entrada.nextDouble();
+		double MEDIA;
+		MEDIA = (n1*2 + n2*3 + n3*5)/10;
+		System.out.printf("MEDIA = %.1f\n",MEDIA);
+    }
+   
+} 
